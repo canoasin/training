e48a046a0910f084e1d12a0b2ffd19b8f4ab6ec3
canoasin <50690804+canoasin@users.noreply.github.com>
2022-07-01 11:11:26 +0900

    Update Prototype.js

--- test/Prototype.js
+++ test/Prototype.js
@@ -10,6 +10,8 @@
  * - object 자료처럼 다루면 됨
  * - 값을 여러개 부여할 수 있고 함수도 넣을 수 있음
  * - prototype에 추가된 데이터들은 자식들이 직접 가지는 개념이 아니라 부모만 가지고 있음
+ * - .prototype : 부모의 유전자를 조작
+ * - __proto__ : 자식이 부모의 유전자를 조작하여 상속
 */
 
 /** 
@@ -32,3 +34,56 @@ var arr = new Array(1,2,3);
  * constructor : 자식들이 값을 직접 소유하게 만들고 싶을 때
  * prototype : 부모만 가지고 있고 자식은 참조해서만 사용하게 하고 싶을 때
  */
+
+
+
+
+ /**
+  * 예제 2번
+  * 다음 코드의 출력 결과는 무엇일까요?
+  */
+ 
+
+  function Parent(){
+    this.name = 'Kim';
+  }
+  var a = new Parent();
+  
+  a.__proto__.name = 'Park'; // 부모의 prototype name에 'Park'를 추가하라는 의미
+  console.log(a.name) // -> Kim
+ 
+
+
+
+ /**
+   * 예제 3번
+   * sayHi() 출력이 안되는 이유?
+   */
+ 
+ function Student(이름, 나이){
+   this.name = 이름;
+   this.age = 나이;
+ }
+ 
+ Student.prototype.sayHi = () => {
+   console.log('안녕 나는 ' + this.name + '이야'); // Arrow Function 내에서의 this는 window를 가르키기 때문
+ }
+ 
+ // object안에 들어있는 함수안에 있는 this는 함수를 부른 object가 됨.
+ // 하지만 arrow function의 경우 함수 안에서 this 뜻이 재정의되지 않고 바깥에 있던 this를 사용한다. 
+ 
+ var 학생1 = new Student('Kim', 20);
+ 
+ 학생1.sayHi(); 
+
+
+
+
+ /**
+  * 예제 4번
+  * 모든 array에 적용할 수 있는 함수를 직접 새로 만들려면 어떻게 해야할까요?
+  */
+
+Array.prototype.함수명 = function() {
+  함수내용;
+}
