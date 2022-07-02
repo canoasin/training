29fa1271f41663849383d8ac78f54a08e34c1a24
canoasin <50690804+canoasin@users.noreply.github.com>
2022-07-02 19:35:04 +0900

    Update products.js

--- shop/src/components/products.js
+++ shop/src/components/products.js
@@ -1,7 +1,10 @@
 import { Col } from "react-bootstrap";
 
 export default function Product(props) {
-  let imgNum = props.num + 1;
+  if(props.shoes[props.num].id == 0) {
+    var imgNum = 1;
+  } else imgNum = props.shoes[props.num].id;
+  
   return (
     <Col md={4}>
       {/* <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%"/> */}
