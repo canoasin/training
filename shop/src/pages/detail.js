d0e1ed0881a42147eb26125898e80b05edf027a1
canoasin <50690804+canoasin@users.noreply.github.com>
2022-07-02 20:24:06 +0900

    Update detail.js

--- shop/src/pages/detail.js
+++ shop/src/pages/detail.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Nav } from "react-bootstrap";
 
 let YellowBtn = styled.button`
   background: ${(props) => props.bg};
@@ -13,27 +14,27 @@ let YellowBtn = styled.button`
 `; */
 
 export default function Detail(props) {
-
   let [banner, setBanner] = useState(true);
   let [count, setCount] = useState(0);
-  let [qty, setQty] = useState('');
+  let [qty, setQty] = useState("");
   let { id } = useParams();
   let imgNum = parseInt(id) + 1;
+  let [tab, setTab] = useState(0);
 
   useEffect(() => {
     let a = setTimeout(() => {
       setBanner(false);
     }, 2000);
 
-    // clean up function : 타이머 등 사용할 때 
+    // clean up function : 타이머 등 사용할 때
     return () => {
       clearTimeout(a);
     }; // useEffect 내에 있는 코드보다 먼저 동작
   }, []); // [] 입력 시 mount시에만 동작함
 
   useEffect(() => {
-    if(isNaN(qty) == true) {
-      alert('그러지마세요');
+    if (isNaN(qty) == true) {
+      alert("그러지마세요");
     }
   }, [qty]);
 
@@ -61,13 +62,73 @@ export default function Detail(props) {
           />
         </div>
         <div className="col-md-6">
-          <input onChange={(e) => {setQty(e.target.value)}} />
+          <input
+            onChange={(e) => {
+              setQty(e.target.value);
+            }}
+          />
           <h4 className="pt-5">{props.shoes[id].title}</h4>
           <p>{props.shoes[id].content}</p>
           <p>{props.shoes[id].price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              setTab(0);
+            }}
+            eventKey="link0"
+          >
+            상품정보
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              setTab(1);
+            }}
+            eventKey="link1"
+          >
+            배송정보
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              setTab(2);
+            }}
+            eventKey="link2"
+          >
+            문의
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent tab={tab} />
+    </div>
+  );
+}
+
+function TabContent({ tab }) {
+  /* if (tab == 0) {
+    return <div>Made In Germany</div>;
+  }
+  if (tab == 1) {
+    return <div>CJ대한통운 2일 내 배송</div>;
+  }
+  if (tab == 2) {
+    return <div>준비중입니다.</div>;
+  } */
+  return (
+    <div className="start">
+      {
+        [
+          <div>Made In Germany</div>,
+          <div>CJ대한통운 2일 내 배송</div>,
+          <div>준비중입니다.</div>,
+        ][tab]
+      }
     </div>
   );
 }
